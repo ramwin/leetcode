1ab4e4a7a7ea21ea3c9897ac2e2b8ca5c47b7b25
Xiang Wang <ramwin@qq.com>
2016-05-25 15:17:54 +0800

    spiral-matrix submitted

--- spiral-matrix.py
+++ spiral-matrix.py
@@ -7,10 +7,56 @@
 
 class Solution(object):
 
+    def spiralOrder2(self, matrix):
+        ''' 耗时: 48ms'''
+        result = []
+        up = 0
+        left = 0
+        right = 0
+        down = 0
+        try:
+            m = len(matrix) # 行数
+            n = len(matrix[0])  # 列数
+        except:
+            return []
+        while up + down < m and right + left < n:
+            print('上')
+            print(left)
+            # print(m-right)
+            # print(matrix[up][left:n-right])
+            result += matrix[up][left:n-right]
+            up += 1
+            print(result)
+            # 最后一列
+            if up + down < m and right + left < n:
+                for i in range(up, m-down):
+                    print('右')
+                    result.append(matrix[i][n-right-1])
+                right += 1
+                print(result)
+            # 最后一行
+            if up + down < m and right + left < n:
+                print(right)
+                print(left)
+                for i in range(n-right-1, left-1, -1):
+                    print('下')
+                    result.append(matrix[m-down-1][i])
+                down += 1
+            print(result)
+            # 第一列
+            if up + down < m and right + left < n:
+                for i in range(m-down-1, up-1, -1):
+                    print('左')
+                    result.append(matrix[i][left])
+                left += 1
+        return result
+        
+
     def spiralOrder(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: List[int]
+        耗时:   64ms
         """
         self.matrix = matrix
         self.result = []
@@ -32,5 +78,5 @@ class Solution(object):
         self.matrix = self.tmp_out
 
 a = Solution()
-print(a.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+print(a.spiralOrder2([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
 
