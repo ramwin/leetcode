bb07267e9a7662c90e8981e003121b0f4c250bf4
wangxiang <wangx@ubuntu1704.mac>
2017-05-16 18:24:45 +0800

    rising_temperature

--- rising_temperature.sql
+++ rising_temperature.sql
@@ -1,13 +1,13 @@
 use leetcode;
-drop table rising_temperature;
-create table rising_temperature (
+drop table Weather;
+create table Weather (
     Id integer,
     Date date not null,
     Temperature integer
 );
-insert into rising_temperature values(1, "2015-01-16", 3);
-insert into rising_temperature values(2, "2015-01-15", -1);
--- insert into rising_temperature values(3, "2015-01-03", 20);
--- insert into rising_temperature values(4, "2015-01-04", 30);
+insert into Weather values(1, "2015-01-16", 3);
+insert into Weather values(2, "2015-01-15", -1);
+-- insert into Weather values(3, "2015-01-03", 20);
+-- insert into Weather values(4, "2015-01-04", 30);
 
-select b.Id from rising_temperature a, rising_temperature b where a.Date = b.Date-1 and a.Temperature < b.Temperature;
+select b.Id from Weather a, Weather b where b.Date = DATE_ADD(a.Date, INTERVAL 1 DAY) and a.Temperature < b.Temperature;
