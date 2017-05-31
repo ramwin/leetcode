1aad27aaeb73f32f034cfb0c6f10cd28eef7b0f5
wangxiang <wangx@ubuntu1704.mac>
2017-05-31 17:05:56 +0800

    add two numbers

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -3,6 +3,9 @@
 # Xiang Wang @ 2017-05-31 15:51:37
 
 
+# https://leetcode.com/problems/add-two-numbers/#/description
+
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
@@ -17,13 +20,13 @@ class Solution(object):
         if l1 is None and l2 is None:  # 不能都为空了
             return None
         if l1 is None:
-            s = 0 + l2.val
+            l1 = ListNode(0)
         elif l2 is None:
-            s = l1.val + 0
-        else:
-            s = l1.val + l2.val
+            l2 = ListNode(0)
+        s = l1.val + l2.val
         if s >= 10:
-            add = True
+            add = 1
+            s = s - 10
             # l3 = ListNode(s-10)
             # if l1.next is None and l2.next is None:
             #     l3.next = ListNode(1)
@@ -35,11 +38,19 @@ class Solution(object):
             #     l3.next = self.addTwoNumbers(l1.next, l2.next)
 
         else:
+            add = 0
             # l3 = ListNode(s)
             # if l1 is None:
             #     l3.next = self.addTwoNumbers(None, l2.next)
             # if l2 is None:
             #     l3.next = self.addTwoNumbers(l1.next, None)
+        l3 = ListNode(s)
+        if add:
+            if l1.next is None:
+                l1.next = ListNode(1)
+            else:
+                l1.next.val += 1
+        l3.next = self.addTwoNumbers(l1.next, l2.next)
         return l3
 
 
