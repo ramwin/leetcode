9ab02c2942ce8a275e9bbd9b2f8f77c2f3b51202
Xiang Wang <ramwin@qq.com>
2021-09-02 22:58:03 +0800

    整数反转.py

---
+++ 整数反转.py
@@ -0,0 +1,10 @@
+class Solution:
+
+    def reverse(self, x: int) -> int:
+        if x >= 0:
+            x = int(str(x)[::-1])
+        else:
+            x = -int(str(x)[-1:0:-1])
+        if -2**31 <= x <= 2**31 - 1:
+            return x
+        return 0
